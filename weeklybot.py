9ca7f9c889f97aefd3bc8c95711de17e343d98b8
Adam Zhang <azha3852@uni.sydney.edu.au>
2022-07-08 22:51:10 +1000

    Tested how to send images with the bot. Currently working on having the bot send scheduled messages.

--- weeklybot.py
+++ weeklybot.py
@@ -3,3 +3,15 @@ import asyncio
 from datetime import datetime
 from discord.ext import tasks, commands
 
+TOKEN = "OTk0OTQxODk4NTg4NDM4NTI5.GAo6zJ.lB9k_RyfMIkbhhZrXwJzcW9ZfV-PRzmCYEw5Ik"
+
+command_prefix = "."
+bot = commands.Bot(command_prefix=command_prefix)
+
+@bot.command()
+async def bird(ctx):
+    with open("b03.jpeg", "rb") as b:
+        bird = discord.File(b)
+        await ctx.send(file=bird)
+
+bot.run(TOKEN)
