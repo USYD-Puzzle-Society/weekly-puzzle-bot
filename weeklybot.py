d8da795f31a79c1e483fe4bc7ea2e4c5ec8406d3
Adam Zhang <azha3852@uni.sydney.edu.au>
2022-07-31 13:15:01 +1000

    Made all fields inline.

--- weeklybot.py
+++ weeklybot.py
@@ -104,13 +104,13 @@ async def help(ctx):
         embed_msg.add_field(name="Setup", value="".join(help_setup), inline=True)
         embed_msg.add_field(name="Description", value="".join(help_setup_desc), inline=True)
 
-        embed_msg.add_field(name="Start", value="".join(help_start), inline=False)
+        embed_msg.add_field(name="Start", value="".join(help_start), inline=True)
         embed_msg.add_field(name="Description", value="".join(help_start_desc), inline=True)
 
-        embed_msg.add_field(name="Stop", value="".join(help_stop), inline=False),
+        embed_msg.add_field(name="Stop", value="".join(help_stop), inline=True),
         embed_msg.add_field(name="Description", value="".join(help_stop_desc), inline=True)
 
-    embed_msg.add_field(name="Other", value="".join(help_other), inline=False)
+    embed_msg.add_field(name="Other", value="".join(help_other), inline=True)
     embed_msg.add_field(name="Other", value="".join(help_other_desc), inline=True)
 
     await ctx.send(embed=embed_msg)
