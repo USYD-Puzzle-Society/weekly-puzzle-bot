425e1ae801374d577c35e00cbd37aae9cf49dcf1
Adam Zhang <adamzhang.aus@gmail.com>
2022-08-18 00:00:15 +1000

    Calculations added for text formatting for gunpoint command that should make the text fit on the image better

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -62,24 +62,38 @@ class Reactions(commands.Cog):
         img_filename = f"{self.reactions_dir}/guns_at_{'_'.join(args)}.png"
 
         # default font size is 128 but decreases based on how many characters there are
-        font_size = 128
-        font_decr_val = -5 # how much the font size will be decreased for each character over the limit
-        char_lim = 5 # the amount of characters that will be taken before the font size decreases 
+        default_font_size = 128
         text = " ".join(args)
-        text_len = len(text)
 
-        amt_exceed = text_len - char_lim
-        if 0 < amt_exceed:
-            font_size = font_size + (font_decr_val * amt_exceed)
+        start_box = (240, 270) # the starting coordinates of the "box" which the text will be bound by
+        end_box = (600, 320)
+        mid_y = (start_box[1] - end_box[1])/2
+        box_length = start_box[0] - end_box[0]
 
-        # make sure that font size has not decreased past zero
-        if 0 > font_size:
-            font_size = 3
+        dist_to_rat_head = 20 # the x coordinate difference to the rat head (rougly) from the start x of the box
 
         img = Image.open(f"{self.reactions_dir}/guns_at_rat.png")
         I1 = ImageDraw.Draw(img)
-        font = ImageFont.truetype(font="fonts/Avenir Light.ttf", size=font_size)
-        I1.text((240, 270), text, font=font, stroke_width=2)
+        font = ImageFont.truetype(font="fonts/Avenir Light.ttf", size=default_font_size)
+
+        text_size = I1.textlength(text=text, font=font) # size of string in pixel
+        # check if the string size is larger than the box
+        if text_size > box_length:
+            # decrease font size until the string fits
+            amt_decrease = 0
+            while text_size > box_length:
+                amt_decrease += 1
+                new_font_size = default_font_size - amt_decrease
+                font = ImageFont.truetype(font="fonts/Avenir Light.ttf", size=new_font_size)
+                text_size = I1.textlength(text=text, font=font)
+
+            # the smaller the font, the further down the text will start
+            start_box[1] = round(start_box[1] + (mid_y * amt_decrease/default_font_size))
+        else:
+            # the smaller the number of characters, the closer to the rat head the text will start
+            start_box[0] = round(start_box[0] + (dist_to_rat_head * 1/len(text)))
+
+        I1.text(start_box, text, font=font, stroke_width=2)
 
         # save new image with text
         img.save(img_filename)
