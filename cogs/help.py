55f3d9dd2ab98afb8b2191f433f580f1c7dd5fb8
Adam Zhang <adamzhang.aus@gmail.com>
2022-08-19 22:08:05 +1000

    Fixed text

--- cogs/help.py
+++ cogs/help.py
@@ -69,7 +69,7 @@ class Help(commands.Cog):
         self.help_reactions = [
             ".bird\n\n",
             ".stare\n\n\n",
-            ".a[aaaaaaaaa]\n\n",
+            ".aa[aaaaaaaa]\n\n",
             ".pansive\n\n",
             ".devious\n\n\n",
             ".rubidance\n\n\n",
