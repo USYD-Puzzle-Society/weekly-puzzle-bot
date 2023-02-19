5a61e5b7a3b9c672141bfd4990522e6acb6ba2ae
Adam Zhang <azha3852@uni.sydney.edu.au>
2023-02-19 19:10:11 +1100

    changed release id

--- cogs/start.py
+++ cogs/start.py
@@ -29,7 +29,7 @@ class Start(commands.Cog):
 
         # the release id is used to identify this specific puzzle release
         # if the user wants to stop this release from happening, then the id is used
-        release_id = str(now.second) + str(now.microsecond)
+        release_id = puzz_name
 
         # get the puzzle info
         text = self.info_obj.get_text(ctx, puzz_name, True)
