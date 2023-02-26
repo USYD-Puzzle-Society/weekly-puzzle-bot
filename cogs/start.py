d1d019c523f4f0e830ffe3274d4655a5c283ac18
Adam Zhang <azha3852@uni.sydney.edu.au>
2023-02-27 02:32:09 +1100

    Update start.py

--- cogs/start.py
+++ cogs/start.py
@@ -27,7 +27,7 @@ class Start(commands.Cog):
 
         # the release id is used to identify this specific puzzle release
         # if the user wants to stop this release from happening, then the id is used
-        release_id = puzz_name
+        release_id = datetime.datetime.now().second
 
         if "rc" == puzz_name:
             puzz_name = "rebuscryptic"
