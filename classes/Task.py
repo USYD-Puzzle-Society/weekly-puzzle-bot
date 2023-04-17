b824f16d823039c8469c27cc9e1eb4dc6b890a9e
SkellyBG <skellybg245@gmail.com>
2023-04-17 14:38:15 +1000

    assign, view, delete and add tasks

--- classes/Task.py
+++ classes/Task.py
@@ -1,15 +1,18 @@
 global_id: int = 1
 
 class Task():
-    def __init__(self, task_name = "None", owner = "None", due_date = "None", status = "Unassigned"):
+    def __init__(self, task_name = "None", owners = ["None"], due_date = "None", status = "Unassigned"):
         global global_id
         self.task_id: int = global_id
         global_id += 1
 
         self.task_name: str = task_name
-        self.owner = owner
+        self.owners: "list[str]" = owners
         self.due_date: str = due_date
         self.status: str = status
 
     def to_tuple(self):
-        return (self.task_id, self.task_name, self.owner, self.due_date, self.status)
+        return (self.task_id, self.task_name, self.owners, self.due_date, self.status)
+
+    def owners_to_str(self):
+        return ", ".join(self.owners)
--- cogs/subcom.py
+++ cogs/subcom.py
@@ -28,10 +28,24 @@ class SubcomTasks(commands.Cog):
         ]
         '''
     async def add_task(self, ctx: commands.context.Context, args: "list[str]", role):
-        self.tasks.append(Task(args[0] if args else "None", ctx.author.mention))
+        task = Task(args[0] if args else "None", [ctx.author.mention])
+        self.tasks.append(task)
+        await ctx.send(f"New Task created with Task ID {task.task_id}")
 
     async def view_task(self, ctx: commands.context.Context, args: "list[str]", role):
-        pass
+        if not args:
+            await ctx.send("You must provide a Task ID for viewing!")
+            return
+        to_be_viewed = self.find_task(args[0])
+        if to_be_viewed:
+            embed = discord.Embed(title=f"Task Details for Task {to_be_viewed.task_id}", color=discord.Color.greyple())
+            embed.add_field(name="Task ID", value=to_be_viewed.task_id, inline=False)
+            embed.add_field(name="Task Name", value=to_be_viewed.task_name, inline=False)
+            embed.add_field(name="Owner", value=to_be_viewed.owner, inline=False)
+            embed.add_field(name="Due Date", value=to_be_viewed.due_date, inline=False)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"Task {args[0]} not found!")
 
     async def view_all_tasks(self, ctx: commands.context.Context, args: "list[str]", role):
         '''
@@ -41,35 +55,55 @@ class SubcomTasks(commands.Cog):
         embed = discord.Embed(title="All Active Tasks", color=discord.Color.greyple())
         values = list(map(list, zip(*[task.to_tuple() for task in self.tasks]))) # cursed
         if not values:
-            values = ["nothing!", "empty", "much wow"]
-        embed.add_field(name="Tasks", value="\n".join((['. '.join(x) for x in zip([str(x) for x in values[0]], values[1])])).strip("\n"))
-        embed.add_field(name="Owner", value="\n".join(values[2]).strip())
-        embed.add_field(name="Due Date", value="\n".join(values[3]).strip())
+            values = ["0.", "nothing!", "empty", "much wow"]
+        embed.add_field(name="Tasks", value="\n".join((['. '.join(x) for x in zip([str(x) for x in values[0]], values[1])])))
+        embed.add_field(name="Owner", value="\n".join([", ".join(owners) for owners in values[2]]))
+        embed.add_field(name="Due Date", value="\n".join(values[3]))
 
         await ctx.send(embed=embed)
     
     async def assign_task(self, ctx: commands.context.Context, args: "list[str]", role):
-        pass
+        '''
+        if args is empty, assign task to self
+        otherwise, take a list of mentions
+        '''
+        if not args:
+            await ctx.send("You must provide a Task ID for assignment!")
+            return
+        task = self.find_task(args[0])
+        if not task:
+            await ctx.send(f"Task {args[0]} not found!")
+            return
+        mentions = args[1:]
+        if not mentions:
+            task.owners = [ctx.author.mention]
+        else:
+            task.owners = args[1:]
+        await ctx.send(f"Task {task.task_id} assigned to {task.owners_to_str()}")
     
     async def delete_task(self, ctx: commands.context.Context, args: "list[str]", role):
         if not args:
             await ctx.send("You must provide a Task ID for deletion!")
             return
-        to_be_deleted = None
-        for task in self.tasks:
-            if str(task.task_id) == args[0]:
-                to_be_deleted = task
+        to_be_deleted = self.find_task(args[0])
         if to_be_deleted:
             self.tasks.remove(to_be_deleted)
             await ctx.send(f"Task {to_be_deleted.task_id} successfully deleted.")
         else:
             await ctx.send(f"Task {args[0]} not found!")
     
+    def find_task(self, task_id: int) -> Task:
+        result = None 
+        for task in self.tasks:
+            if (str(task.task_id) == task_id):
+                result  = task
+        return result
+    
     @commands.command()
     @commands.has_any_role(exec_role, subcom_role)
     async def task(self, ctx: commands.context.Context, *args):
         if len(args) < 2 or args[0] not in ["subcom", "exec"] or args[1] not in ["add", "view", "viewall", "assign", "delete"]:
-            await ctx.send("Please use the command in the form `.task [subcom/exec] [add/view/assign/delete]`")
+            await ctx.send("Please use the command in the form `.task [subcom/exec] [add/view/viewall/assign/delete]`")
             return
         role = discord.utils.get(ctx.guild.roles, name=exec_role)
         if args[0] == "exec" and role not in ctx.author.roles():
@@ -77,15 +111,15 @@ class SubcomTasks(commands.Cog):
             return
         
         if args[1] == "add":
-            await self.add_task(ctx, list(args)[2:], args[1])
+            await self.add_task(ctx, list(args)[2:], args[0])
         elif args[1] == "view":
-            await self.view_task(ctx, list(args)[2:], args[1])
+            await self.view_task(ctx, list(args)[2:], args[0])
         elif args[1] == "viewall":
-            await self.view_all_tasks(ctx, list(args)[2:], args[1])
+            await self.view_all_tasks(ctx, list(args)[2:], args[0])
         elif args[1] == "assign":
-            pass
+            await self.assign_task(ctx, list(args)[2:], args[0])
         elif args[1] == "delete":
-            pass
+            await self.delete_task(ctx, list(args)[2:], args[0])
         else:
             pass
 
