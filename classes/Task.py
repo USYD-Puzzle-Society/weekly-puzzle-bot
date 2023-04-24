43452bd55841c2b6ddb152771ebd5e320c833013
Adam Zhang <azha3852@uni.sydney.edu.au>
2023-04-25 01:58:40 +1000

    moving from_dict to subcom.py

--- classes/Task.py
+++ classes/Task.py
@@ -40,18 +40,18 @@ class Task():
 
         return res
     
-def from_dict(data):
-    task = Task()
-    task.task_id = data["task_id"]
-    task.task_name = data["task_name"]
-    task.owner = data["owner"]
-    task.contributors = data["contributors"]
-    task.creation_date = datetime.date.fromisoformat(data["creation_date"])
-    task.due_date = datetime.date.fromisoformat(data["due_date"])
-    task.status = data["status"]
-    task.description = data["description"]
-    task.comments = data["comments"]
+# def from_dict(data):
+#     task = Task()
+#     task.task_id = data["task_id"]
+#     task.task_name = data["task_name"]
+#     task.owner = data["owner"]
+#     task.contributors = data["contributors"]
+#     task.creation_date = datetime.date.fromisoformat(data["creation_date"])
+#     task.due_date = datetime.date.fromisoformat(data["due_date"])
+#     task.status = data["status"]
+#     task.description = data["description"]
+#     task.comments = data["comments"]
 
-    return task
+#     return task
     
 
