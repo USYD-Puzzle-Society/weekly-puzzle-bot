3d77ed3e3afa2d6bc0f0f4034eeb67c1211d1dac
SkellyBG <skellybg245@gmail.com>
2023-05-31 23:48:56 +1000

    add listener for app script's message

--- cogs/weeklypuz.py
+++ cogs/weeklypuz.py
@@ -13,6 +13,7 @@ FORM_API = ""
 
 class WeeklyPuz(commands.Cog):
     def __init__(self, bot):
+        self.bot = bot
         pass
 
     @commands.command()
@@ -41,6 +42,13 @@ class WeeklyPuz(commands.Cog):
             await ctx.send('Form submitted successfully.')
         else:
             await ctx.send('Form submission failed.')
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id != 1100077444922359959:
+            return
+        await self.bot.get_channel(994948949536407612).send("Top 3 Taken!")
+        
 
 
 async def setup(bot: commands.Bot):
