ee58e43c0e06d03a3cd05f9a2b2933602ca35384
SkellyBG <skellybg245@gmail.com>
2023-07-16 00:50:39 +1000

    Revert "add storing of user id to name for weekly puz"
    This reverts commit 832e21629d21a2aa72df8e6c374a5fd7226a648a.

--- cogs/weeklypuz.py
+++ cogs/weeklypuz.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 import os.path
 import requests
 from info import Info
-import json
-from json import JSONDecodeError
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,33 +10,25 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-WEEKLYPUZ_FILE = "weeklypuz_name.json"
+FORM_API = ""
 
 class WeeklyPuz(commands.Cog):
     def __init__(self, bot, info: Info):
         self.bot = bot
         self.info = info
-        self.names: "dict[str, str]" = {}
-        try:
-            with open(WEEKLYPUZ_FILE, "r") as f:
-                self.names = json.load(f)
-        except FileNotFoundError:
-            open(WEEKLYPUZ_FILE, "x").close()
-        except JSONDecodeError:
-            pass
 
     @commands.command()
     async def weeklypuz(self, ctx: commands.context.Context, *args):
-        if len(args) < 1 or args[1] not in ["help", "answer", "register"]:
-            await ctx.send("Please use the command in the form `.weeklypuz [help/answer/register] ...")
+        if len(args) < 1 or args[1] not in ["help", "answer", "setup"]:
+            await ctx.send("Please use the command in the form `.weeklypuz [help/answer/setup] ...")
             return
         
         if args[1] == "help":
             pass
         elif args[1] == "answer":
             pass
-        elif args[1] == "register":
-            self.register(ctx, args[1:])
+        elif args[1] == "setup":
+            pass
 
     @commands.command()
     async def answer(self, ctx: commands.context.Context, *args):
@@ -53,21 +43,6 @@ class WeeklyPuz(commands.Cog):
             await ctx.send('Form submitted successfully.')
         else:
             await ctx.send('Form submission failed.')
-
-    async def register(self, ctx: commands.context.Context, args: "list[str]"):
-        if len(args) < 1:
-            await ctx.send("Please register in the form `.weeklypuz register <name>`. Your name can contain multiple words.")
-            return
-        self.names[ctx.author] = " ".join(args)
-        
-        try:
-            with open(WEEKLYPUZ_FILE, "w") as f:
-                json.dump(f)
-        except:
-            await ctx.send("Something went wrong - contact the developer of this bot.")
-            return
-
-        await ctx.send(f"Success! You are now registered as {self.names[ctx.author]}!")
 
     @commands.Cog.listener()
     async def on_message(self, message):
