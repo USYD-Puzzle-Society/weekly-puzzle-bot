b3d7beda7a6fca1d99552b8fd364894860d9d2b3
SkellyBG <skellybg245@gmail.com>
2023-08-18 23:11:32 +1000

    use double-quote string for docstrings

--- src/subcom_task.py
+++ src/subcom_task.py
@@ -2,23 +2,22 @@ from classes.Task import Task
 from db import db
 
 def new_task(owner: str, task_name: str = "None") -> Task:
-    '''
+    """
     Given an owner and an optional task name, add a new Task object to the database and return that object.
 
-    Args:
+    Args:   
         owner: Owner of the task.
         task_name: Name of the task.
 
     Returns:
         A new Task object
-
-    '''
+    """
     task = Task(owner=owner, task_name=task_name)
     task = db.create_task(task)
     return task
 
 def view_task(task_id: int) -> Task:
-    '''
+    """
     Given a task id, retrieve the corresponding Task, or None if no task with such id exist.
 
     Args:
@@ -26,13 +25,13 @@ def view_task(task_id: int) -> Task:
     
     Returns:
         The Task object with the matching id.
-    '''
+    """
 
     task = db.retrieve_task_by_id(task_id)
     return task
 
 def view_all_tasks(view_archive: bool = False) -> "list[Task]":
-    '''
+    """
     Retrieve all archived or unarchived Task objects.
 
     Args:
@@ -41,11 +40,11 @@ def view_all_tasks(view_archive: bool = False) -> "list[Task]":
         
     Returns:
         A list of Tasks that fulfills the condition.
-    '''
+    """
     return db.retrieve_tasks()
 
 def archive_task(task_id: int) -> None:
-    '''
+    """
     Given a task id, archive the corresponding Task.
 
     Args:
@@ -53,13 +52,13 @@ def archive_task(task_id: int) -> None:
 
     Returns:
         None.
-    '''
+    """
     task = db.retrieve_task_by_id(task_id)
     task.archived = True
     db.update_task(task_id, task)
 
 def delete_task(task_id: int) -> None:
-    '''
+    """
     Given a task id, delete the corresponding Task, regardless of whether it's archived or not.
 
     Args:
@@ -67,7 +66,7 @@ def delete_task(task_id: int) -> None:
 
     Returns:
         None.
-    '''
+    """
     db.delete_task(task_id)
 
 def clear():
