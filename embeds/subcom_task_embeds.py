b3dceae3b040efd5c391efc8ff2d69378d4bc021
SkellyBG <skellybg245@gmail.com>
2023-08-21 19:30:41 +1000

    fix indentation in tasks_list_view_embed

--- embeds/subcom_task_embeds.py
+++ embeds/subcom_task_embeds.py
@@ -17,21 +17,21 @@ def task_view_embed(task: Task) -> discord.Embed:
     return embed
 
 def tasks_list_view_embed(tasks: "list[Task]", view_archive) -> discord.Embed:
-        if view_archive:
-            title = "All Archived Tasks"
-        else:
-            title = "All Active Tasks"
+    if view_archive:
+        title = "All Archived Tasks"
+    else:
+        title = "All Active Tasks"
 
-        embed = discord.Embed(title=title, color=discord.Color.greyple())
-        values = list(map(list, zip(*[task.summary_to_tuple() for task in tasks]))) # cursed
+    embed = discord.Embed(title=title, color=discord.Color.greyple())
+    values = list(map(list, zip(*[task.summary_to_tuple() for task in tasks]))) # cursed
 
-        if not values:
-            embed.add_field(name="Tasks", value="")
-            embed.add_field(name="Owner", value="")
-            embed.add_field(name="Due Date", value="")
-        else:
-            embed.add_field(name="Tasks", value="\n".join((['. '.join(x) for x in zip([str(x) for x in values[0]], values[1])])))
-            embed.add_field(name="Owner", value="\n".join(values[2]))
-            embed.add_field(name="Due Date", value="\n".join([time.isoformat() for time in values[3]] ))
-        
-        return embed
+    if not values:
+        embed.add_field(name="Tasks", value="")
+        embed.add_field(name="Owner", value="")
+        embed.add_field(name="Due Date", value="")
+    else:
+        embed.add_field(name="Tasks", value="\n".join((['. '.join(x) for x in zip([str(x) for x in values[0]], values[1])])))
+        embed.add_field(name="Owner", value="\n".join(values[2]))
+        embed.add_field(name="Due Date", value="\n".join([time.isoformat() for time in values[3]] ))
+    
+    return embed
