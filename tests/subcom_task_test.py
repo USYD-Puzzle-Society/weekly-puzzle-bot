7de950692595da615a98435a020953d6917be987
SkellyBG <skellybg245@gmail.com>
2023-08-21 19:31:08 +1000

    write additional tests for subcom_task functions

--- tests/subcom_task_test.py
+++ tests/subcom_task_test.py
@@ -1,4 +1,8 @@
+import asyncio
+import pytest
+
 from src.subcom_task import *
+from src.subcom_task_errors import TaskNotFoundError
 
 def setup_function():
     clear()
@@ -17,3 +21,25 @@ def test_create_multiple_tasks():
 def test_view_task():
     task = new_task('alice')
     assert task == view_task(task.task_id)
+
+def test_view_all_tasks():
+    tasks = []
+    tasks.append(new_task('alice', 'Write EOS Hunt'))
+    tasks.append(new_task('bob', 'Purchase Prizes'))
+    assert tasks == view_all_tasks(view_archive=False)
+
+def test_archive_task():
+    task = new_task('alice')
+    archive_task(task.task_id)
+    task = view_task(task.task_id)
+    assert task.archived == True
+    assert len(view_all_tasks()) == 0
+    assert len(view_all_tasks(view_archive=True)) == 1
+    assert view_all_tasks(view_archive=True)[0] == task
+
+def test_delete_task():
+    task = new_task('alice')
+    delete_task(task.task_id)
+    assert len(view_all_tasks()) == 0
+    with pytest.raises(TaskNotFoundError):
+        view_task(task.task_id)
