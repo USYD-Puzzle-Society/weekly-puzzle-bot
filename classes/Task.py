74fda3839885e9f847b9c5c1a5e0a3629637e0ad
SkellyBG <skellybg245@gmail.com>
2023-08-24 02:20:09 +1000

    add documentation for Task model

--- classes/Task.py
+++ classes/Task.py
@@ -3,6 +3,22 @@ from pydantic import BaseModel
 from typing import List, Optional, Tuple
 
 class Task(BaseModel):
+    """A task in the task board.
+
+    Attributes:
+        task_id: A sequentially generated integer representing the task's ID. May be omitted, in which case
+            it means the task has not been added to the database.
+        task_name: A string representing the task's name.
+        owner: A string representing the task's owner.
+        contributors: A list of string representing the task's contributors.
+        description: A string representing the task's description.
+        comments: A string representing the task's comments.
+        creation_date: A Datetime object representing the creation date of the task. Note that microseconds are set to 0
+            due to a limitation in how MongoDB store Datetime.
+        due_date: A Datetime object representing the due date of the task.
+        archived: A boolean indicating whether the task has been archived.
+        archived_date: A Datetime object representing the archival date of the task.
+    """
     task_id: Optional[int] = None
     task_name: str = "None"
     owner: str = "None"
@@ -17,4 +33,10 @@ class Task(BaseModel):
     archived_date: Optional[datetime] = None
 
     def get_summary(self) -> Tuple[int, str, str, datetime]: 
+        """
+            Retrieve a summary of the task, for use in a task board.
+
+            Returns:
+                A tuple consisting of the task ID, task name, owner and due date.
+        """
         return (self.task_id, self.task_name, self.owner, self.due_date)
