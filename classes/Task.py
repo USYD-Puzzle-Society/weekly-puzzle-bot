0b2132829a9117d50bda9a8800d595745d10ad00
SkellyBG <skellybg245@gmail.com>
2023-08-23 19:42:55 +1000

    use Optional instead of Union[.., None]

--- classes/Task.py
+++ classes/Task.py
@@ -1,9 +1,9 @@
 from datetime import datetime 
 from pydantic import BaseModel
-from typing import List, Union, Tuple
+from typing import List, Optional, Tuple
 
 class Task(BaseModel):
-    task_id: Union[int, None] = None
+    task_id: Optional[int] = None
     task_name: str = "None"
     owner: str = "None"
     contributors: List[str] = ["None"]
@@ -12,9 +12,9 @@ class Task(BaseModel):
     comments: str = "None"
 
     creation_date: datetime = datetime.now().replace(microsecond=0)
-    due_date: Union[datetime, None] = None
+    due_date: Optional[datetime] = None
     archived: bool = False
-    archived_date: Union[datetime, None] = None
+    archived_date: Optional[datetime] = None
 
     def get_summary(self) -> Tuple[int, str, str, datetime]: 
         return (self.task_id, self.task_name, self.owner, self.due_date)
