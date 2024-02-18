62616f44c4a58954f215f1e75460692f92b9e79d
Adam Zhang <adamzhang.aus@gmail.com>
2024-02-19 00:25:15 +1100

    typo again

--- cogs/setup.py
+++ cogs/setup.py
@@ -141,7 +141,7 @@ class Setup(commands.Cog):
     @commands.command()
     @commands.has_role("Executives")
     async def qsetweek(self, ctx: commands.context.Context, preset: str, week_num: str):
-        preset, accepted = self.check_reset(preset)
+        preset, accepted = self.info_obj.check_preset(preset)
         if not accepted:
             await ctx.send(f"Please use one of the accepted presets: {', '.join(self.default_presets)}")
             return
