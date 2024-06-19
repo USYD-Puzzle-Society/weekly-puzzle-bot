d6cffe90ff6b3f879025a5087a256dab9f0bf79d
Tom Hughes <78639733+phoenix-31@users.noreply.github.com>
2024-06-19 12:16:05 +1000

    Created scheduler cog - this won't work first try

---
+++ cogs/required/scheduler.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+class Scheduler(commands.Cog, AsyncIOScheduler):
+    def __init__(self, bot: commands.Bot, info):
+        super().__init__()
+        self.start()
+        self.bot = bot
+        self.info = info
+
+    async def start_puzzle(self, puzzle_name):
+        puzzle = self.info.puzzles[puzzle_name]
+        channel = self.bot.get_channel(puzzle.release_channel)
+        channel.send(puzzle.get_text())
+
+    def schedule_puzzle(self, puzzle_name, datetime):
+        self.add_job(self.start_puzzle, "date", run_date=datetime, args=[puzzle_name])
+    
+async def setup(bot: commands.Bot):
+    info = bot.get_cog("Info")
+    await bot.add_cog(Scheduler(bot, info), guild=discord.Object(1153319575048437833))
