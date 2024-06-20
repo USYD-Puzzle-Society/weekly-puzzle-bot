e4888c93b9d00e4f2bfc64c0dd847fdc30b696d1
Tom Hughes <78639733+phoenix-31@users.noreply.github.com>
2024-06-20 15:03:00 +1000

    Updated show command for new classes

--- cogs/optional/show.py
+++ cogs/optional/show.py
@@ -8,28 +8,27 @@ from discord.ext import commands
 class Show(commands.GroupCog, group_name="show"):
     def __init__(self, bot: commands.Bot, info):
         self.bot = bot
-        self.info_obj = info
+        self.info = info
 
     @discord.app_commands.command(
         name="puzzle"
     )
     @commands.has_role("Executives")
     async def show(self, interaction: discord.Interaction, puzzle_name: str):
-        puzzle_name = await self.info_obj.check_puzzle_name(interaction, puzzle_name)
+        puzzle_name = await self.info.check_puzzle_name(interaction, puzzle_name)
         if not puzzle_name:
             return
     
-        puzzle_info = self.info_obj.info[puzzle_name]
-        text = self.info_obj.get_text(interaction, False, puzzle_name)
+        puzzle = self.info.puzzles[puzzle_name]
+        text = puzzle.get_text(interaction.guild, False)
 
         await interaction.response.send_message(
-            f"The following will be released at "
-            + f"{puzzle_info["release_datetime"]} "
-            + f"in <#{puzzle_info["channel_id"]}>."
+            f"The following will be released at {puzzle.release_time} in "
+            + f"<#{puzzle.release_channel}>."
         )
         await interaction.channel.send(text)
-        for i in range(len(puzzle_info["img_urls"])):
-            await interaction.channel.send(puzzle_info["img_urls"][i])
+        for i in range(len(puzzle.image_urls)):
+            await interaction.channel.send(puzzle.image_urls[i])
 
 
 async def setup(bot: commands.Bot):
