26d96856d5c55675f96439b1007938f1765b2692
Tom Hughes <78639733+phoenix-31@users.noreply.github.com>
2024-06-20 21:18:55 +1000

    Split set puzzle into component edit commands

--- cogs/optional/setup.py
+++ cogs/optional/setup.py
@@ -1,4 +1,3 @@
-from typing import Callable
 import discord
 from discord.ext import commands
 
@@ -10,8 +9,11 @@ class Setup(commands.GroupCog, group_name="set"):
         self.info = info
         self.datetime_format = "%d/%m/%Y %H:%M"
 
-    async def get_image_urls(self, interaction: discord.Interaction, message_from_user: Callable):
-        await interaction.channel.send(
+    async def get_image_urls(self, interaction: discord.Interaction):
+        def message_from_user(msg):
+            return msg.author == interaction.user
+        
+        await interaction.response.send_message(
             "Please send all the images for the puzzle in one message."
             + " Type `exit` at any time to stop."
         )
@@ -37,47 +39,9 @@ class Setup(commands.GroupCog, group_name="set"):
         return [image.url for image in msg.attachments]
 
     @discord.app_commands.command(
-        name="puzzle"
-    )
-    @commands.has_role("Executives")
-    async def set_puzzle(
-            self, interaction: discord.Interaction, puzzle_name: str, 
-            submission_link: str, interactive_link: str = ""):
-        puzzle_name = await self.info.check_puzzle_name(interaction, puzzle_name)
-        if not puzzle_name:
-            return
-
-        await interaction.response.send_message(
-            f"Puzzle setup started. The puzzle that you are setting has a {puzzle_name} puzzle classification."
-        )
-
-        puzzle = self.info.puzzles[puzzle_name]
-
-        def message_from_user(msg):
-            return msg.author == interaction.user
-
-        image_urls = await self.get_image_urls(interaction, message_from_user)
-        if image_urls == []:
-            return
-        
-        puzzle.image_urls = image_urls
-        puzzle.submission_link = submission_link
-        puzzle.interactive_link = interactive_link
-        self.info.save()
-
-        text = puzzle.get_text(interaction.guild, False)
-        
-        await interaction.channel.send(
-            f"Done. The following will be released at {puzzle.release_time} in "
-            + f"<#{puzzle.release_channel}>. It will mention the role `{role_name}`."
-        )
-        await interaction.channel.send(text)
-        for i in range(len(image_urls)):
-            await interaction.channel.send(image_urls[i])
-
-    @discord.app_commands.command(
         name="role"
     )
+    @commands.has_role("Executives")
     async def set_role(
             self, interaction: discord.Interaction, puzzle_name: str,
             role_name: str):
@@ -100,6 +64,7 @@ class Setup(commands.GroupCog, group_name="set"):
     @discord.app_commands.command(
         name="channel"
     )
+    @commands.has_role("Executives")
     async def set_channel(
             self, interaction: discord.Interaction, puzzle_name: str, 
             channel_id: str):
@@ -124,8 +89,8 @@ class Setup(commands.GroupCog, group_name="set"):
     )
     @commands.has_role("Executives")
     async def set_time(
-            self, interaction: discord.Interaction, puzzle_name: str, date: str, 
-            time: str): 
+            self, interaction: discord.Interaction, puzzle_name: str, 
+            date: str, time: str): 
         puzzle_name = await self.info.check_puzzle_name(interaction, puzzle_name)
         if not puzzle_name:
             return
@@ -168,6 +133,65 @@ class Setup(commands.GroupCog, group_name="set"):
         )
 
         puzzle.week = week
+        self.info.save()
+
+    @discord.app_commands.command(
+        name="images"
+    )
+    @commands.has_role("Executives")
+    async def set_images(self, interaction: discord.Interaction, puzzle_name: str):
+        puzzle_name = await self.info.check_puzzle_name(interaction, puzzle_name)
+        if not puzzle_name:
+            return
+
+        puzzle = self.info.puzzles[puzzle_name]
+
+        image_urls = await self.get_image_urls(interaction)
+        if image_urls == []:
+            return
+
+        if len(image_urls) == 1:
+            await interaction.channel.send(
+                "The new image link for the puzzle is "
+                + f"{image_urls[0]}."
+            )
+        else:
+            await interaction.channel.send(
+                "The new image links for the puzzle are "
+                + f"{', '.join(image_urls)}."
+            )
+
+        puzzle.image_urls = image_urls
+        self.info.save()
+
+    @discord.app_commands.command(
+        name="links"
+    )
+    @commands.has_role("Executives")
+    async def set_links(
+            self, interaction: discord.Interaction, puzzle_name: str, 
+            submission_link: str, interactive_link: str = ""):
+        puzzle_name = await self.info.check_puzzle_name(interaction, puzzle_name)
+        if not puzzle_name:
+            return
+
+        puzzle = self.info.puzzles[puzzle_name]
+
+        message = ("The previous submission link for the puzzle was "
+            + f"{puzzle.submission_link}. "
+            + "The new submission link for the puzzle is "
+            + f"{submission_link}.")
+        
+        if interactive_link:
+            message += ("\n\nThe previous interactive link for the puzzle was "
+                + f"{puzzle.interactive_link}. "
+                + "The new interactive link for the puzzle is "
+                + f"{interactive_link}.")
+        
+        await interaction.response.send_message(message)
+
+        puzzle.submission_link = submission_link
+        puzzle.interactive_link = interactive_link
         self.info.save()
 
 
