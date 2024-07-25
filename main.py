9b4aaba7c63ac528bdbda28c97b3e6f9e8ba0059
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-07-26 00:03:50 +1000

    feat: added commands to sync and clear slash commands

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ command_prefix = "."
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=command_prefix, help_command=None, intents=intents)
 
+
 # load all available cogs on startup
 @bot.command()
 @commands.has_role(exec_id)
@@ -25,12 +26,14 @@ async def startup(ctx: commands.context.Context):
             print(f"Loaded {filename}")
     await ctx.send(f"Loaded all cogs")
 
+
 # command to load a cog
 @bot.command()
 @commands.has_role(exec_id)
 async def load(ctx: commands.context.Context, extension):
     await bot.load_extension(f"{cogs_dir}.{extension}")
     await ctx.send(f"Loaded {extension} cog")
+
 
 # command to unload a cog
 @bot.command()
@@ -39,11 +42,34 @@ async def unload(ctx: commands.context.Context, extension):
     await bot.unload_extension(f"{cogs_dir}.{extension}")
     await ctx.send(f"Unloaded {extension} cog")
 
+
 # command to reload a cog
 @bot.command()
 @commands.has_role(exec_id)
 async def reload(ctx: commands.context.Context, extension):
     await bot.reload_extension(f"{cogs_dir}.{extension}")
     await ctx.send(f"Reloaded {extension} cog")
+
+
+@bot.command()
+async def sync(ctx: commands.context.Context):
+    try:
+        bot.tree.copy_global_to(guild=ctx.guild)
+        synced = await bot.tree.sync(guild=ctx.guild)
+        await ctx.send(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
+
+
+@bot.command()
+async def clear(ctx: commands.context.Context):
+    bot.tree.clear_commands(guild=ctx.guild)
+
+    try:
+        await bot.tree.sync()
+        await ctx.send("Commands cleared.")
+    except Exception as e:
+        print(e)
+
 
 bot.run(TOKEN)
