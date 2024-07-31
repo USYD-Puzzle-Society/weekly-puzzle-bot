0608322a2f67b504f0ded812827e10aece05ff17
Adam Zhang <azha3852@uni.sydney.edu.au>
2024-08-01 00:51:00 +1000

    feat: rewrote show to use slash commands

--- cogs/show.py
+++ cogs/show.py
@@ -1,114 +1,116 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
 import os
 import json
-import discord
-import datetime
-from info import Info
-from discord.ext import commands
 
-class Show(commands.Cog):
-    def __init__(self, bot: commands.Bot, info: Info):
+from typing import Literal
+
+EXEC_ROLE_NAME = "Executives"
+
+
+class Show(commands.GroupCog):
+    def __init__(self, bot: commands.bot):
         self.bot = bot
 
-        self.info_obj = info
-    
-    """
-    Fix to have only one method
-    """
+        self.info_fp = "info.json"
 
-    @commands.command()
-    @commands.has_role("Executives")
-    async def show(self, ctx: commands.context.Context, preset: str):
-        preset, accepted = self.info_obj.check_preset(preset)
-        if not accepted:
-            await ctx.send(f"Please use one of the accepted presets: {', '.join(self.info_obj.default_presets)}")
+    @app_commands.command(
+        name="wpc",
+        description="Shows the current stored info for the respective WPC puzzle.",
+    )
+    @commands.has_role(EXEC_ROLE_NAME)
+    async def show_wpc(
+        self,
+        interaction: discord.Interaction,
+        release_day: Literal["Monday", "Wednesday", "Friday"],
+    ):
+        await interaction.response.defer()
+
+        # get the info from info.json
+        if os.path.exists(self.info_fp):
+            with open(self.info_fp, "r") as f:
+                info = json.load(f)
+        # error checking
+        else:
+            await interaction.followup.send(
+                "No puzzles have been setup. Use `/setup wpc` to set the puzzles."
+            )
             return
-    
-        puzz_info = self.info_obj.info[preset]
-        puzz_text = self.info_obj.get_qtext(ctx, False, preset)
-        puzz_time = puzz_info["release_datetime"]
-        puzz_channel = puzz_info["channel_id"]
 
-        await ctx.send(f"The following will be released at {puzz_time} in <#{puzz_channel}>:")
-        await ctx.send(puzz_text)
-        for i in range(len(puzz_info["img_urls"])):
-            await ctx.send(puzz_info["img_urls"][i])
+        try:
+            puzz_data = info[f"{release_day}WPC"]
+        except KeyError:
+            await interaction.followup.send(
+                "No puzzles have been setup. Use `/setup wpc` to set the puzzles."
+            )
+            return
 
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showrc(self, ctx: commands.context.Context):
-        rc_info = self.info_obj.info["rebuscryptic"]
-        rc_text = self.info_obj.get_rebuscryptic_text(ctx, False)
-        rc_time = rc_info["release_datetime"]
-        rc_channel = rc_info["channel_id"]
+        if puzz_data["releasing"]:
+            await interaction.followup.send(
+                f"The following will be released at {puzz_data['release_datetime']} in <#{puzz_data['channel_id']}>:"
+            )
+        else:
+            await interaction.followup.send(
+                f"The following will be released in <#{puzz_data['channel_id']}>. "
+                + f"The release time is set for {puzz_data['release_datetime']} but the release has not been started yet. "
+                + f"Do `/start wpc {release_day}` to start the release."
+            )
+        await interaction.channel.send(
+            f"@/{interaction.guild.get_role(puzz_data['role_id'])}"
+            + puzz_data["release_text"]
+        )
 
-        await ctx.send(f"The following will be released at {rc_time} in <#{rc_channel}>")
-        await ctx.send(rc_text)
-        for i in range(len(rc_info["img_urls"])):
-            await ctx.send(rc_info["img_urls"][i])
+        for img in puzz_data["img_urls"]:
+            await interaction.channel.send(img)
 
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showminipuzz(self, ctx: commands.context.Context):
-        puzz_info = self.info_obj.info["minipuzz"]
-        puzz_text = self.info_obj.get_minipuzz_text(ctx, False)
-        puzz_time = puzz_info["release_datetime"]
-        puzz_channel = puzz_info["channel_id"]
+    @app_commands.command(
+        name="jff",
+        description="Shows the current stored info for the respective JFF puzzle.",
+    )
+    @commands.has_role(EXEC_ROLE_NAME)
+    async def show_jff(
+        self,
+        interaction: discord.Interaction,
+        release_day: Literal["Monday", "Friday"],
+    ):
+        await interaction.response.defer()
 
-        await ctx.send(f"The following will be released at {puzz_time} in <#{puzz_channel}>.")
-        await ctx.send(puzz_text)
-        for i in range(len(puzz_info["img_urls"])):
-            await ctx.send(puzz_info["img_urls"][i])
+        # get the info from info.json
+        if os.path.exists(self.info_fp):
+            with open(self.info_fp, "r") as f:
+                info = json.load(f)
+        # error checking
+        else:
+            await interaction.followup.send(
+                "No puzzles have been setup. Use `/setup jff` to set the puzzles."
+            )
+            return
 
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showcrossword(self, ctx: commands.context.Context):
-        crossword_info = self.info_obj.info["crossword"]
-        crossword_text = self.info_obj.get_crossword_text(ctx, False)
-        crossword_time = crossword_info["release_datetime"]
-        crossword_channel = crossword_info["channel_id"]
+        try:
+            puzz_data = info[f"{release_day}JFF"]
+        except KeyError:
+            await interaction.followup.send(
+                "No puzzles have been setup. Use `/setup jff` to set the puzzles."
+            )
+            return
 
-        await ctx.send(f"The following will be released at {crossword_time} in <#{crossword_channel}>")
-        await ctx.send(crossword_text)
-        for i in range(len(crossword_info["img_urls"])):
-            await ctx.send(crossword_info["img_urls"][i])
+        if puzz_data["releasing"]:
+            await interaction.followup.send(
+                f"The following will be released at {puzz_data['release_datetime']} in <#{puzz_data['channel_id']}>:"
+            )
+        else:
+            await interaction.followup.send(
+                f"The following will be released in <#{puzz_data['channel_id']}>. "
+                + f"The release time is set for {puzz_data['release_datetime']} but the release has not been started yet. "
+                + f"Do `/start wpc {release_day}` to start the release."
+            )
+        await interaction.channel.send(puzz_data["release_text"])
 
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showwordsearch(self, ctx: commands.context.Context):
-        wordsearch_info = self.info_obj.info["wordsearch"]
-        wordsearch_text = self.info_obj.get_wordsearch_text(ctx, False)
-        wordsearch_time = wordsearch_info["release_datetime"]
-        wordsearch_channel = wordsearch_info["channel_id"]
+        for img in puzz_data["img_urls"]:
+            await interaction.channel.send(img)
 
-        await ctx.send(f"The following will be released at {wordsearch_time} in <#{wordsearch_channel}>")
-        await ctx.send(wordsearch_text)
-        for i in range(len(wordsearch_info["img_urls"])):
-            await ctx.send(wordsearch_info["img_urls"][i])
-
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showlogicpuzz(self, ctx: commands.context.Context):
-        logicpuzz_info = self.info_obj.info["logicpuzz"]
-        logicpuzz_text = self.info_obj.get_logicpuzz_text(ctx, False)
-        logicpuzz_time = logicpuzz_info["release_datetime"]
-        logicpuzz_channel = logicpuzz_info["channel_id"]
-
-        await ctx.send(f"The following will be released at {logicpuzz_time} in <#{logicpuzz_channel}>")
-        await ctx.send(logicpuzz_text)
-        for i in range(len(logicpuzz_info["img_urls"])):
-            await ctx.send(logicpuzz_info["img_urls"][i])
-
-    @commands.command()
-    @commands.has_role("Executives")
-    async def showciyk(self, ctx: commands.context.Context):
-        ciyk_info = self.info_obj.info["ciyk"]
-        ciyk_text = self.info_obj.get_ciyk_text(ctx, False)
-        ciyk_time = ciyk_info["release_datetime"]
-        ciyk_channel = ciyk_info["channel_id"]
-
-        await ctx.send(f"The following will be set at {ciyk_time} in <#{ciyk_channel}>.")
-        await ctx.send(ciyk_text)
 
 async def setup(bot: commands.Bot):
-    info = Info()
-    await bot.add_cog(Show(bot, info))
+    await bot.add_cog(Show(bot))
